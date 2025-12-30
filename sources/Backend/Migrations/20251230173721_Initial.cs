f22016b599a759d810a08712962713fb32ad431d
bessonov <nikwoochach@yandex.ru>
2025-12-31 03:03:20 +0500

    Fix namespace

--- sources/Backend/Migrations/20251230173721_Initial.cs
+++ sources/Backend/Migrations/20251230173721_Initial.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Backend.Migrations
+namespace Migrations
 {
     /// <inheritdoc />
     public partial class Initial : Migration
